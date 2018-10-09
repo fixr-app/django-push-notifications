5158dbc1341ca1f1d5293ab41a9ef10e04099150
Hunterx7 <ialbetsram@gmail.com>
2018-10-09 16:23:58 +0100

    Added fixr driver

--- setup.py
+++ setup.py
@@ -3,4 +3,9 @@
 from setuptools import setup
 
 
-setup()
+setup(
+    name = 'django-push-notifications',
+    download_url="https://github.com/jleclanche/django-push-notifications/tarball/master",
+	url="https://github.com/jleclanche/django-push-notifications",
+	version="1.6.0"
+)
